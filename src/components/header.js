d6d8a065c695e4f4981ae6f536a2e2c8f37eb8c4
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2025-03-12 19:51:22 +0530

    dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -6,30 +6,39 @@ import pic from "../assets/logo.png";
 export default function Header() {
     const [drop, setDrop] = useState(false);
 
+    const handleLinkClick = () => {
+        setDrop(false);
+    };
+
     return (
         <nav className="navbar">
-            <div className="logo">
+            <Link to="/"><div className="logo">
                 <img src={pic} height="50px" alt="logo" /> ANES
-            </div>
+            </div></Link>
             <div className="nav-links">
-                <Link to="/">Home</Link>
-                <Link to="/register" className="sign-in">Donate</Link>
-                <Link to="/getallData">Request Blood</Link>
-                <Link to="/about" className="sign-in">About</Link>
-                <Link to="/hall-of-fame" className="sign-in">Hall of Fame</Link>
+                <Link to="/" onClick={handleLinkClick}>Home</Link>
+                <Link to="/register" className="sign-in" onClick={handleLinkClick}>Donate</Link>
+                <Link to="/getallData" onClick={handleLinkClick}>Request Blood</Link>
+                <Link to="/about" className="sign-in" onClick={handleLinkClick}>About</Link>
+                <Link to="/hall-of-fame" className="sign-in" onClick={handleLinkClick}>Hall of Fame</Link>
             </div>
+
             <div className="extend">
                 <div className="inner" onClick={() => setDrop(!drop)}>
-                    {drop ? "X" : "☰"}
+                    ☰
                 </div>
             </div>
-            <div className={`sidemenu ${drop ? "active" : ""}`}>
-                <div className="link"><Link to="/">Home</Link></div>
-                <div className="link"><Link to="/register" className="sign-in">Donate</Link></div>
-                <div className="link"><Link to="/getallData">Request Blood</Link></div>
-                <div className="link"><Link to="/about" className="sign-in">About</Link></div>
-                <div className="link"><Link to="/hall-of-fame" className="sign-in">Hall of Fame</Link></div>
-            </div>
+
+            {/* Sidebar Menu */}
+            {drop && (
+                <div className={`sidemenu active`}>
+                    <div className="link"><Link to="/" onClick={handleLinkClick}>Home</Link></div>
+                    <div className="link"><Link to="/register" className="sign-in" onClick={handleLinkClick}>Donate</Link></div>
+                    <div className="link"><Link to="/getallData" onClick={handleLinkClick}>Request Blood</Link></div>
+                    <div className="link"><Link to="/about" className="sign-in" onClick={handleLinkClick}>About</Link></div>
+                    <div className="link"><Link to="/hall-of-fame" className="sign-in" onClick={handleLinkClick}>Hall of Fame</Link></div>
+                </div>
+            )}
         </nav>
     );
 }
