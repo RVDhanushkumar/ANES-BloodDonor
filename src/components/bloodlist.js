b70bb6a1e9abb59a08ff1e4c4e9d0f293e01c1f5
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2025-03-13 22:44:52 +0530

    validation update

--- src/components/bloodlist.js
+++ src/components/bloodlist.js
@@ -7,17 +7,19 @@ const BloodList = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [blood, setBlood] = useState("All");
-  const [alldata,setalldata] = useState([]);
+  const [alldata, setalldata] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://blood-ey76.onrender.com/user/getAllEntries");
+        const response = await fetch(
+          "https://blood-ey76.onrender.com/user/getAllEntries"
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
         const result = await response.json();
-        setalldata(result);
+        setalldata(result.filter((donor) => donor.isVerified === true));
         console.log(alldata);
       } catch (error) {
         setError(error.message);
@@ -30,16 +32,21 @@ const BloodList = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-        setData(blood === "All" ? alldata : alldata.filter(donor => donor.bloodgroup === blood));
-      }
+      setData(
+        blood === "All"
+          ? alldata
+          : alldata.filter((donor) => donor.bloodgroup === blood)
+      );
+    };
     fetchData();
-  }, [blood,alldata]);
+  }, [blood, alldata]);
 
-  if (loading) return <div className="loading">
-    <div className="shimmer">
-        Loading...
-    </div>
-  </div>;
+  if (loading)
+    return (
+      <div className="loading">
+        <div className="shimmer">Loading...</div>
+      </div>
+    );
   if (error) return <div className="error">Error: {error}</div>;
 
   return (
@@ -47,20 +54,26 @@ const BloodList = () => {
       <BloodRequest />
       <h1 className="title">Available Donors</h1>
       <div className="filter-container">
-        <label><span style={{textShadow:"0px 0px 3px white"}}>🟢 </span>LIVE</label>
+        <label>
+          <span style={{ textShadow: "0px 0px 3px white" }}>🟢 </span>LIVE
+        </label>
         <div>
-        <label htmlFor="blood-group">Filter by Blood Group:</label>
-        <select id="blood-group" value={blood} onChange={(e) => setBlood(e.target.value)}>
-          <option value="All">All</option>
-          <option value="A+">A+</option>
-          <option value="A-">A-</option>
-          <option value="B+">B+</option>
-          <option value="B-">B-</option>
-          <option value="O+">O+</option>
-          <option value="O-">O-</option>
-          <option value="AB+">AB+</option>
-          <option value="AB-">AB-</option>
-        </select>
+          <label htmlFor="blood-group">Filter by Blood Group:</label>
+          <select
+            id="blood-group"
+            value={blood}
+            onChange={(e) => setBlood(e.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+          </select>
         </div>
       </div>
       <div className="table-wrapper">
@@ -71,8 +84,6 @@ const BloodList = () => {
               <th>Age</th>
               <th>Gender</th>
               <th>Blood Group</th>
-              <th>Mobile</th>
-              <th>Email</th>
               <th>Address</th>
             </tr>
           </thead>
@@ -83,8 +94,6 @@ const BloodList = () => {
                 <td>{user.age}</td>
                 <td>{user.gender}</td>
                 <td>{user.bloodgroup}</td>
-                <td>{user.mobile}</td>
-                <td>{user.email}</td>
                 <td>{user.address}</td>
               </tr>
             ))}
